0db485cf455a651a9c72814d98bc0a5bae3485a8
Julien Kuzniarek <julienkuzniarek@gmail.com>
2020-10-07 16:58:38 -0400

    added wordlist

---
+++ projects/MSS/wordlist.php
@@ -0,0 +1,283 @@
+<?php 
+include 'config.php';
+include $sRoot.'templates/head.php';
+include $sRoot.'templates/sidebar.php';
+
+?>
+<div class="row py-3"> <!-- Title Row-->
+  <div class="col-sm-4"></div>
+  <div class="col-sm-8 border-bottom border-dark">
+    <h1><em>MSS</em><?=$icon?></h1>
+  </div>
+</div>
+<br>
+<div class="row"> <!-- Content Row-->
+  <div class="col-lg-10">
+    <a href="introduction.php" class="btn btn-light">Introduction</a>
+    <a href="pronunciation.php" class="btn btn-light">Pronunciation</a>
+    <a href="sample.php" class="btn btn-light">Sample</a>
+    <a href="wordlist.php" class="btn btn-light active">Fundamental Words</a>
+    <hr>
+
+    <p>
+      Below are several lists containing the most fundamental words in English. 
+      Knowing only these words (and how to use them) is sufficient for ordinary communication in English. 
+      These lists were retrieved from <a href="http://ogden.basic-english.org/words.html">Odgen's Basic English Vocabulary</a> prior to their respelling in MSS.
+    </p>
+    <h3>OPERATIONS - 100 words</h3>
+    <br>
+    <div class="row">
+      <div class="col">
+        <h4>MSS</h4>
+        <p>
+          kum, get, giv, goe, keep, let, mayk, puut, seem, tayk, bee, du, haav, say, see, send, may, wil,
+          about, akross, aaftur, agenst, amung, aat, bifor, bitween, by, doun, frum, in, of, on, oevur, thru, tu, undur, up, with,
+          az, for, uv, til, then,
+          a, tha, ol, eni, evri, litl, much, noe, uthur, sum, such, thaat, thiss, Y, hee, yu, hu, 
+          end, bekuz, but, or, if, thoe, wyl, how, wen, weir, wy,
+          agen, evur, far, forwurd, heeur, neeur, now, out, stil, then, their, tugether, wel,
+          ulmoest, anuf, eevin, not, oenli, qwyt, soe, veiri, tumaro, yesturday,
+          north, south, eest, west, pleez, yess.
+        </p>
+      </div>
+      <div class="col">
+        <h4>English</h4>
+        <p>
+          come, get, give, go, keep, let, make, put, seem, take, be, do, have, say, see, send, may, will,
+          about, across, after, against, among, at, before, between, by, down, from, in, off, on, over, through, to, under, up, with,
+          as, for, of, till, than,
+          a , the, all, any, every, little, much, no, other, some, such, that, this, I, he, you, who,
+          and, because, but, or, if, though, while, how, when, where, why,
+          again, ever, far, forward, here, near, now, out, still, then, there, together, well,
+          almost, enough, even, not, only, quite, so, very, tomorrow, yesterday,
+          north, south, east, west, please, yes.
+        </p>
+      </div>
+    </div>
+
+    <h3>THINGS - 400 General words</h3>
+    <br>
+    <div class="row">
+      <div class="col">
+        <h4>MSS</h4>
+        <p>
+          akount, aakt, adishin, adjustmint, aadvurtyzmint, agreemint, air, amount, ameuzmint, aanimal,
+          aansur, aapuraatis, aprooval, argeumint, art, ataak, atempt, atenshin, atrakshin, athoriti,
+          baak, baalinss, bayss, bihayvyur, bileef, burth, bit, byt, blud, bloe, 
+          bodi, braass, bred, breth, bruthur, bilding, burn, burst, bizniss, butur, 
+          kaanviss, kair, koz, chok, chaanss, chaynj, kloth, koel, kulur, kumfurt, 
+          kumiti, kumpeni, kumpairisin, kompatishin, kundishin, kunekshin, kuntroel, kuuk, kopur, kopi, 
+          kork, kotin, kof, kuntri, kuvur, krak, kredit, krym, krush, kry, 
+          kurint, kurv, daamij, daynjur, dotur, day, deth, det, disizhin, digri, 
+          dizyn, dizyur, distrukshin, deetayl, divelupmint, dijeschin, direkshin, diskuvuri, diskushin, dizeez, 
+          disgust, distinss, distribeushin, divizhin, dout, dreenk, dryving, dust, urth, ej, 
+        </p>
+        <p>
+          ejookayshin, ifekt, end, eirur, ivent, ixaampl, ixchaynj, igzistinss, ikspanshin, ixpeeureeinss, 
+          expurt, faakt, fol, faamili, fothur, feeur, feeling, fikshin, feeld, fyt, 
+          fyur, flaym, flyt, flouur, foeld, food, forss, form, frend, frunt, 
+          froot, glaass, goeld, guvurnmint, grayn, graass, grip, groop, groeth, gyd,
+          harbur, harmuni, hayt, heeuring, heet, help, histuri, hoel, hoep, ouur, 
+          heumur, yss, ydeea, impulss, inkreess, industri, eenk, insekt, instrumint, inshurinss, 
+          intrist, invenshin, y'urn, jeli, join, jurni, juj, jump, kik, kiss, 
+          nolij, laand, layngwij, laaf, law, leed, lurning, lethur, letur, levl, 
+          lift, lyt, limit, linin, likwid, list, luuk, loss, luv, masheen, 
+          maan, maanajer, mark, markit, maass, meel, mezhur, meet, meeting, memuri,
+        </p>
+        <p>
+          metl, midl, milk, mynd, myn, minit, mist, muni, munth, morning,
+          muthur, moeshin, mountin, moov, meuzik, naym, nayshin, need, nooz, nyt,
+          noiz, noet, numbur, obsurvayshin, ofur, oil, opurayshin, upinyin, ordur, orginyzayshin,
+          ornamint, oenur, payj, payn, paynt, paypur, part, payst, paymint, peess, 
+          pursin, playss, plaant, play, plezhur, point, poizin, polish, portur, poezishin,
+          poudur, pouur, pryss, print, prosess, proedooss, profit, propurti, proez, proetest,
+          puul, punishmint, purpuss, puush, qwoliti, qweschin, rayn, raynj, rayt, ray,
+          reeaakshin, reeding, reezin, rekurd, rigret, rilayshin, rilijin, reprizentativ, riqwest, rispekt,
+          rest, riword, rithim, ryss, rivur, roed, roel, room, rub, rool, 
+          run, solt, saand, skayl, syinss, see, seet, sekritairi, silekshin, self, 
+        </p>
+        <p>
+          senss, survint, sex, shayd, shayk, shaym, shok, syd, syn, silk, 
+          silvur, sistur, syz, sky, sleep, slip, sloep, smash, smel, smyl, 
+          smoek, sneez, snoe, soep, susyiti, sun, song, sort, sound, soop,
+          spayss, stayj, start, staytmint, steem, steel, step, stich, stoen, stop, 
+          stori, strech, strukchur, substinss, shuugur, sugjeschin, sumur, suport, supryz, swim, 
+          sistim, tok, tayst, tax, teeching, tendinsi, test, theeuri, thing, thot,
+          thundur, tym, tin, top, tuch, trayd, tranzport, trik, trubl, turn, 
+          twist, eunit, euz, vaaleu, vurss, vesul, veu, voiss, wok, wor, 
+          waash, wayst, wotur, wayv, waax, way, wethur, week, wayt, wind,
+          wyn, wintur, wuumin, wuud, wuul, wurd, wurk, wound, ryting, yeeur.
+        </p>
+      </div>
+      <div class="col">
+        <h4>English</h4>
+        <p>
+          account, act, addition, adjustment, advertisement, agreement, air, amount, amusement, animal, 
+          answer, apparatus, approval, argument, art, attack, attempt, attention, attraction, authority, 
+          back, balance, base, behavior, belief, birth, bit, bite, blood, blow, body, brass, bread, breath, 
+          brother, building, burn, burst, business, butter, canvas, care, cause, chalk, chance, change, 
+          cloth, coal, color, comfort, committee, company, comparison, competition, condition, connection, 
+          control, cook, copper, copy, cork, cotton, cough, country, cover, crack, credit, crime, crush, 
+          cry, current, curve, damage, danger, daughter, day, death, debt, decision, degree, design, desire, 
+          destruction, detail, development, digestion, direction, discovery, discussion, disease, disgust, 
+          distance, distribution, division, doubt, drink, driving, dust, earth, edge, 
+        </p>
+        <p>
+          education, effect, 
+          end, error, event, example, exchange, existence, expansion, experience, expert, fact, fall, family, 
+          father, fear, feeling, fiction, field, fight, fire, flame, flight, flower, fold, food, force, 
+          form, friend, front, fruit, glass, gold, government, grain, grass, grip, group, growth, guide, 
+          harbor, harmony, hate, hearing, heat, help, history, hole, hope, hour, humor, ice, idea, impulse, 
+          increase, industry, ink, insect, instrument, insurance, interest, invention, iron, jelly, join, 
+          journey, judge, jump, kick, kiss, knowledge, land, language, laugh, law, lead, learning, leather, 
+          letter, level, lift, light, limit, linen, liquid, list, look, loss, love, machine, man, manager, 
+          mark, market, mass, meal, measure, meat, meeting, memory, 
+        </p>
+        <p>
+          metal, middle, milk, mind, mine, minute, 
+          mist, money, month, morning, mother, motion, mountain, move, music, name, nation, need, news, night, 
+          noise, note, number, observation, offer, oil, operation, opinion, order, organization, ornament, 
+          owner, page, pain, paint, paper, part, paste, payment, peace, person, place, plant, play, pleasure, 
+          point, poison, polish, porter, position, powder, power, price, print, process, produce, profit, 
+          property, prose, protest, pull, punishment, purpose, push, quality, question, rain, range, rate, 
+          ray, reaction, reading, reason, record, regret, relation, religion, representative, request, respect, 
+          rest, reward, rhythm, rice, river, road, roll, room, rub, rule, run, salt, sand, scale, science, 
+          sea, seat, secretary, selection, self, 
+        </p>
+        <p>
+          sense, servant, sex, shade, shake, shame, shock, side, sign, 
+          silk, silver, sister, size, sky, sleep, slip, slope, smash, smell, smile, smoke, sneeze, snow, 
+          soap, society, son, song, sort, sound, soup, space, stage, start, statement, steam, steel, step, 
+          stitch, stone, stop, story, stretch, structure, substance, sugar, suggestion, summer, support, 
+          surprise, swim, system, talk, taste, tax, teaching, tendency, test, theory, thing, thought, thunder, 
+          time, tin, top, touch, trade, transport, trick, trouble, turn, twist, unit, use, value, verse, 
+          vessel, view, voice, walk, war, wash, waste, water, wave, wax, way, weather, week, weight, wind, 
+          wine, winter, woman, wood, wool, word, work, wound, writing, year.
+        </p>
+      </div>
+    </div>
+
+    <h3>THINGS - 200 Picturable words</h3>
+    <br>
+    <div class="row">
+      <div class="col">
+        <h4>MSS</h4>
+        <p>
+          ayngl, aant, aapl, arch, arm, armi, baybi, baag, bol, baand, 
+          baysin, baaskit, baath, bed, bee, bel, beiri, burd, blayd, bord, 
+          boet, boen, buuk, boot, botl, box, boy, brayn, brayk, braanch, 
+          brik, brij, brush, bukit, bulb, butin, kayk, kaamura, kard, kart,
+          kairij, kaat, chayn, cheez, chest, chin, church, surkl, klok, kloud, 
+          koet, kolur, koem, kord, kow, kup, kurtin, kuushin, dog, dor,
+          drayn, drar, dress, drop, eeur, eg, enjin, y, fayss, farm, 
+          fethur, fingur, fish, flaag, flor, fli, fuut, fork, fouul, fraym, 
+          gardin, gurl, gluv, goet, gun, hair, haamur, haand, haat, hed, 
+          hart, huuk, horn, horss, hospital, houss, ylind, jool, ketl, kee, 
+        </p>
+        <p>
+          nee, nyf, not, leef, leg, lybrairi, lyn, lip, lok, maap, 
+          maach, munki, moon, mouth, musl, nayl, nek, needl, nurv, net, 
+          noez, nut, ofiss, orinj, uvin, parsil, pen, pensil, pikchur, pig, 
+          pin, pyp, playn, playt, plow, pokit, pot, poetaytoe, prisin, pump, 
+          rayl, raat, riseet, ring, rod, roof, root, sayl, skool, sizurz, 
+          skru, seed, sheep, shelf, ship, shurt, shu, skin, skurt, snayk, 
+          sok, spayd, spunj, spoon, spring, skwair, staamp, star, stayshin, stem, 
+          stik, stoking, stumich, stor, street, sun, taybl, tayl, thred, throet, 
+          thum, tikit, toe, tung, tooth, toun, trayn, tray, tree, trouzurz, 
+          umbrela, wol, wotch, weel, wip, wisul, windoe, wing, wyur, wurm.
+        </p>
+      </div>
+      <div class="col">
+        <h4>English</h4>
+        <p>
+          angle, ant, apple, arch, arm, army, baby, bag, ball, band, 
+          basin, basket, bath, bed, bee, bell, berry, bird, blade, board, 
+          boat, bone, book, boot, bottle, box, boy, brain, brake, branch, 
+          brick, bridge, brush, bucket, bulb, button, cake, camera, card, cart, 
+          carriage, cat, chain, cheese, chest, chin, church, circle, clock, cloud, 
+          coat, collar, comb, cord, cow, cup, curtain, cushion, dog, door, 
+          drain, drawer, dress, drop, ear, egg, engine, eye, face, farm, 
+          feather, finger, fish, flag, floor, fly, foot, fork, fowl, frame, 
+          garden, girl, glove, goat, gun, hair, hammer, hand, hat, head, 
+          heart, hook, horn, horse, hospital, house, island, jewel, kettle, key, 
+        </p>
+        <p>
+          knee, knife, knot, leaf, leg, library, line, lip, lock, map, 
+          match, monkey, moon, mouth, muscle, nail, neck, needle, nerve, net, 
+          nose, nut, office, orange, oven, parcel, pen, pencil, picture, pig, 
+          pin, pipe, plane, plate, plough/plow, pocket, pot, potato, prison, pump, 
+          rail, rat, receipt, ring, rod, roof, root, sail, school, scissors, 
+          screw, seed, sheep, shelf, ship, shirt, shoe, skin, skirt, snake, 
+          sock, spade, sponge, spoon, spring, square, stamp, star, station, stem, 
+          stick, stocking, stomach, store, street, sun, table, tail, thread, throat, 
+          thumb, ticket, toe, tongue, tooth, town, train, tray, tree, trousers, 
+          umbrella, wall, watch, wheel, whip, whistle, window, wing, wire, worm.
+        </p>
+      </div>
+    </div>
+
+    <h3>QUALITIES - 100 General</h3>
+    <br>
+    <div class="row">
+      <div class="col">
+        <h4>MSS</h4>
+        <p>
+          aybl, assid, ayngri, otoemaatik, beutiful, blak, boiling, bryt, broekin, broun, 
+          cheep, kemikuul, cheef, kleen, kleeur, komin, komplex, konshiss, kut, deep, 
+          dipendint, urli, ilaastik, ilektrik, eeqwul, faat, furtil, furst, fixt, flaat, 
+          free, freeqwint, ful, jenurul, guud, grayt, gray, haynging, haapi, hard, 
+          helthi, hy, holoe, importint, kynd, lyk, living, long, mayl, maireed, 
+          mateeureeal, medikal, militairi, naachural, nesisairi, nu, normal, oepin, pairalel, paast, 
+          fizikal, pulitikal, por, posibl, prezint, pryvit, probabl, qwik, qwyit, redi, 
+          red, regeulur, risponsibl, ryt, round, saym, sekint, sepurit, seeureeus, sharp, 
+          smooth, stiki, stif, strayt, strong, sudin, sweet, tol, thik, tyt, 
+          tyurd, tru, vylint, wayting, worm, wet, wyd, wyz, yeloe, yung.
+        </p>
+      </div>
+      <div class="col">
+        <h4>English</h4>
+        <p>
+          able, acid, angry, automatic, beautiful, black, boiling, bright, broken, brown, 
+          cheap, chemical, chief, clean, clear, common, complex, conscious, cut, deep, 
+          dependent, early, elastic, electric, equal, fat, fertile, first, fixed, flat, 
+          free, frequent, full, general, good, great, grey/gray, hanging, happy, hard, 
+          healthy, high, hollow, important, kind, like, living, long, male, married, 
+          material, medical, military, natural, necessary, new, normal, open, parallel, past, 
+          physical, political, poor, possible, present, private, probable, quick, quiet, ready, 
+          red, regular, responsible, right, round, same, second, separate, serious, sharp, 
+          smooth, sticky, stiff, straight, strong, sudden, sweet, tall, thick, tight, 
+          tired, true, violent, waiting, warm, wet, wide, wise, yellow, young.
+        </p>
+      </div>
+    </div>
+
+    <h3>QUALITIES - 50 Opposites</h3>
+    <br>
+    <div class="row">
+      <div class="col">
+        <h4>MSS</h4>
+        <p>
+          awayk, baad, bent, bitur, blu, surtin, koeld, kumpleet, krool, dark, 
+          ded, deeur, delikit, difurint, durti, dry, folss, feebl, feemayl, foolish, 
+          feuchur, green, il, laast, layt, left, looss, loud, loe, mixt, 
+          nairoe, oeld, opasit, publik, ruf, saad, sayf, seekrit, short, shut, 
+          simpl, sloe, smol, soft, solid, speshal, straynj, thin, wyt, rong.
+        </p>
+      </div>
+      <div class="col">
+        <h4>English</h4>
+        <p>
+          awake, bad, bent, bitter, blue, certain, cold, complete, cruel, dark, 
+          dead, dear, delicate, different, dirty, dry, false, feeble, female, foolish, 
+          future, green, ill, last, late, left, loose, loud, low, mixed, 
+          narrow, old, opposite, public, rough, sad, safe, secret, short, shut, 
+          simple, slow, small, soft, solid, special, strange, thin, white, wrong. 
+        </p>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+<?php include $sRoot.'templates/footer.php'; ?>
+</html>
