2398b872968637ffdf97b7edb7c64b095bd32220
Julien Kuzniarek <julienkuzniarek@gmail.com>
2021-02-08 16:54:29 -0500

    update layout and styles

--- projects/home.php
+++ projects/home.php
@@ -12,36 +12,39 @@ include $sRoot.'templates/sidebar.php';
 </div>
 <br>
 <div class="row"> <!-- Content Row-->
-  <div class="col-md-12">
-
-        <div class="card bg-none m-3" style="min-width:250px;">
+  <div class="col-md-10">
+    <div class="row">
+      <div class="col">
+        <div class="card bg-none mb-3" style="min-width:250px;">
           <div class="card-body">
             <h3 class="card-title green">EON</h3>
-            <p class="card-text">
+            <p class="card-text mb-0">
               <strong>Extended Object Notation</strong> 
               (EON) is a data description notation and programming language designed as a more user friendly alternative to XML with a JSON-like syntax.
+              <br><a href="EON/" class="btn btn-sm btn-light stretched-link">View</a>
             </p>
-            <hr>
+            <hr class="my-1">
             <small class="badge badge-light">Initiated: 2019</small>
             <small class="badge badge-light">Last Updated: Jan 2021</small>
-            <a href="EON/" class="btn btn-sm btn-light stretched-link float-right">View</a>
           </div>
         </div>
-
-        <div class="card bg-none m-3" style="min-width:250px;">
+      </div>
+      <div class="col">
+        <div class="card bg-none mb-3" style="min-width:250px;">
           <div class="card-body">
           <h3 class="card-title green">MSS</h3>
-            <p class="card-text">
+            <p class="card-text mb-0">
               The <strong>Millennium Spelling System</strong> 
               (MSS) is my attempt at transforming American English into a form where words are truly spelled the way they sound.
+              <br><a href="MSS/" class="btn btn-sm btn-light stretched-link">View</a>
             </p>
-            <hr>
+            <hr class="my-1">
             <small class="badge badge-light">Initiated: 2019</small>
             <small class="badge badge-light">Last Updated: Aug 2020</small>
-            <a href="MSS/" class="btn btn-sm btn-light stretched-link float-right">View</a>
           </div>
         </div>
-
+      </div>
+    </div>
   </div>
 </div>
 
